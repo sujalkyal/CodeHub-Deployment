604c14929fb880df90c7edb3fe1b91f94a7a2b16
sujalkyal <sujalkyal2704@gmail.com>
2025-08-06 00:30:16 +0530

    key number update

--- utils/rapidApiKeyManager.js
+++ utils/rapidApiKeyManager.js
@@ -6,7 +6,9 @@ const apiKeys = [
   { key: process.env.RAPID_API_KEY_2, usage: 0 },
   { key: process.env.RAPID_API_KEY_3, usage: 0 },
   { key: process.env.RAPID_API_KEY_4, usage: 0 },
-  { key: process.env.RAPID_API_KEY_5, usage: 0 }
+  { key: process.env.RAPID_API_KEY_5, usage: 0 },
+  { key: process.env.RAPID_API_KEY_6, usage: 0 },
+  { key: process.env.RAPID_API_KEY_7, usage: 0 }
 ];
 
 // Get the next available API key that hasn't reached the limit
